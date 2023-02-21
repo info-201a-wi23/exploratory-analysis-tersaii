7c744535f714a1f4f2e0c3e7633a46055153ff56
seanmorris1 <98245418+seanmorris1@users.noreply.github.com>
2023-02-20 20:59:24 -0800

    Added Net Tuition Revenue variable

--- UniversityDF.R
+++ UniversityDF.R
@@ -5,9 +5,9 @@ CollegeScorecard <- read_csv("~/exploratory-analysis-tersaii-main/CollegeScoreca
 universityList <- CollegeScorecard[c(3847, 741, 2328, 2997, 772, 763, 1068, 84, 925
                           , 3849), ]
 
-uni <- subset(universityList,select = c('TUITIONFEE_IN', "TUITIONFEE_OUT", "INSTNM", "PAR_ED_PCT_1STGEN", "GRAD_DEBT_MDN", "RET_FT4", "RET_PT4"))
+uni <- subset(universityList,select = c('TUITIONFEE_IN', "TUITIONFEE_OUT", "INSTNM", "PAR_ED_PCT_1STGEN", "GRAD_DEBT_MDN", "RET_FT4", "RET_PT4", "TUITFTE"))
 
-orderedUni <- uni[,c(3, 1, 2, 5, 6, 7, 4)]
+orderedUni <- uni[,c(3, 1, 2, 5, 8, 6, 7, 4)]
 
 orderedUni <- rapply(object = orderedUni, f = round, classes = "numeric", how = "replace", digits = 1)
 
@@ -18,3 +18,4 @@ colnames(orderedUni)[colnames(orderedUni) == "PAR_ED_PCT_1STGEN"] = "First Gener
 colnames(orderedUni)[colnames(orderedUni) == "GRAD_DEBT_MDN"] = "Graduate Student's Debt"
 colnames(orderedUni)[colnames(orderedUni) == "RET_FT4"] = "Full-Time Retention"
 colnames(orderedUni)[colnames(orderedUni) == "RET_PT4"] = "Part-Time Retention"
+colnames(orderedUni)[colnames(orderedUni) == "TUITFTE"] = "Net Tuition Revenue"
