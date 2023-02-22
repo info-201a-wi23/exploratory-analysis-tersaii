51c53eb4e99dd81ceae442b96397f62f71618dda
Aadil Ali <aadil@msn.com>
2023-02-22 00:32:20 -0800

    added debt chart

--- admissions_chart.R
+++ admissions_chart.R
@@ -6,13 +6,12 @@ library("RColorBrewer")
 
 CollegeScorecard <- read_csv("Final_Project/exploratory-analysis-tersaii/CollegeScorecard.csv")
 
+
 universityList <- CollegeScorecard[c(3847, 741, 2328, 2997, 772, 763, 1068, 84, 925
                                      , 3849), ]
 
 admissions_ret <- universityList %>% 
   select("INSTNM", "ADM_RATE", "RET_FT4")
-   
-data_type <- admissions_ret[c(2,3)]
 
 ggplot(admissions_ret, aes(x = ADM_RATE, y = RET_FT4, color = INSTNM)) +
   geom_point(size=6) +
